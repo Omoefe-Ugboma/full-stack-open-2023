43c5a1afb265e6acacf9a1e9ddabf095c6f45cbd
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2023-11-01 22:22:58 +0100

    1.3 Course Information, Step 3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,23 +1,29 @@
-import Header from "./components/Header"
-import Content from "./components/Content"
-import Total from "./components/Content"
-
+ 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = 'Half Stack application development';
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  };
   
   return (
-    <div>
-    <Header course={course} />
-    <Content part1={part1} part2={part2} part3={part3}/>
-    <Total total={exercises1 + exercises2 + exercises3}/>  
-  </div>
+
+      <div>
+      <h1>{course}</h1>
+          <p>{part1.name}: {part1.exercises}</p>
+          <p>{part2.name}: {part2.exercises}</p>
+          <p>{part3.name}: {part3.exercises}</p>
+          <p>Exercises: {part1.exercises + part2.exercises + part3.exercises}</p>
+      </div>
   )
 }
 
-export default App
+export default App;
